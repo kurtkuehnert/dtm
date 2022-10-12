5b7085a4b63f16df300117db8ec15ad391028b0d
Kurt Kühnert <kurt@kuehnert.dev>
2022-10-12 10:13:45 +0200

    fixed pixel size (size in bytes)

--- examples/convert.rs
+++ examples/convert.rs
@@ -1,6 +1,5 @@
 use dtm::DTM;
-use std::path::Path;
-use std::{env, process};
+use std::{env, path::Path, process, time::Instant};
 
 fn main() {
     let args: Vec<_> = env::args().collect();
@@ -13,7 +12,7 @@ fn main() {
     let source = image::open(filein).unwrap();
 
     let descriptor1 = DTM {
-        pixel_size: 10, // TODO: depends on input file
+        pixel_size: 2, // TODO: depends on input file
         channel_count: 1,
         width: source.width() as usize,
         height: source.height() as usize,
@@ -21,5 +20,10 @@ fn main() {
     let data1 = source.as_luma_alpha16().unwrap().to_vec();
 
     let fileout = Path::new(filein).with_extension("dtm");
+
+    let start = Instant::now();
     descriptor1.encode_file(&fileout, &data1).unwrap();
+    let encode = start.elapsed();
+
+    println!("Encoded in: {:?}", encode);
 }
